38799feaed1e04aecddba66d69b475cddadcf509
Herbert Coenen <coenenherbert@gmail.com>
2022-04-06 15:08:24 +0200

    first commit

---
+++ src/main/java/com/switchfully/eurderproject/EurderProjectApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.eurderproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurderProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurderProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/switchfully/eurderproject/EurderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.eurderproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurderProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
