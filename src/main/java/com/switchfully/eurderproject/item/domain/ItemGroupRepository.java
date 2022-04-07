18fe9114715d6bbf276c6d0ec765b34272113508
Herbert Coenen <coenenherbert@gmail.com>
2022-04-07 14:07:35 +0200

    S3 - made test on saving an itemGroup green

--- src/main/java/com/switchfully/eurderproject/item/domain/ItemGroupRepository.java
+++ src/main/java/com/switchfully/eurderproject/item/domain/ItemGroupRepository.java
@@ -1,12 +1,18 @@
 package com.switchfully.eurderproject.item.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Repository
 public class ItemGroupRepository {
+    private final Logger repositoryLogger = LoggerFactory.getLogger(ItemGroupRepository.class);
+
     private final Map<String, ItemGroup> itemGroupById;
 
     public ItemGroupRepository() {
@@ -16,7 +22,12 @@ public class ItemGroupRepository {
         itemGroupById.put(itemGroup.getId(), itemGroup);
     }
 
-    public ItemGroup getById(String id) {
-        return null;
+    public ItemGroup getById(String itemGroupId) {
+        ItemGroup foundItemGroup = itemGroupById.get(itemGroupId);
+        if(foundItemGroup == null) {
+            repositoryLogger.error("No item group could be found for id " + itemGroupId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        return foundItemGroup ;
     }
 }
